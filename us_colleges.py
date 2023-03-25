0e396e890639af0b5479618e0ff7603238465916
matthewsyrigos <122997452+matthewsyrigos@users.noreply.github.com>
2023-03-24 20:56:04 -0400

    Finilised my code for asseignment 2

--- us_colleges.py
+++ us_colleges.py
@@ -6,6 +6,7 @@ wikipedia = MediaWiki()
 college_name = input("Please enter the full-name of the college:")
 college = wikipedia.page(college_name)
 
+#Looking up for the college's data
 def college_lookup():
     """
     This function is used to print the sections with their indices of the college's wikipedia page
@@ -18,6 +19,38 @@ def college_lookup():
     #pprint.pprint(college.sections)
     #print(college.content)
 
+#Morkov Text Analysis
+    """This function analiyses text using the Morkov technique"""
+def generate_frequency_table(text, order):
+    words = text.split()
+    frequency_table = {}
+
+    for i in range(len(words) - order):
+        key = tuple(words[i:i+order])
+        next_word = words[i+order]
+        if key in frequency_table:
+            frequency_table[key].append(next_word)
+        else:
+            frequency_table[key] = [next_word]
+
+    return frequency_table
+
+def generate_markov_text(frequency_table, seed, length):
+    """This function generates text using markov """
+    current_key = seed
+    result = list(current_key)
+    import random
+    for _ in range(length):
+        if current_key in frequency_table:
+            next_word = random.choice(frequency_table[current_key])
+            result.append(next_word)
+            current_key = tuple(result[-len(current_key):])
+        else:
+            break
+
+    return ' '.join(result)
+
+#Analysing the section
 def section_analysis(section_index):
     """
     This function receives a section of a wikipedia page
@@ -56,11 +89,32 @@ def section_analysis(section_index):
 
     bullet_list = format_bullet_list(section_content)
 
-    sentiment = sentiment_analysis(bullet_list)
+    summary = summarize_section(section_content)
+    summary_bullet_list = format_bullet_list(summary)
+    #sentiment = sentiment_analysis(bullet_list)
 
-    return bullet_list, sentiment
+    print("Here is a summary of the content:", summary_bullet_list)
+    
+    return bullet_list
 
+#Creating a summary
+def summarize_section(section_content):
+    """This function receives the selection from 
+    the selection_analysis function and produces a summary"""
+    order = 2
+    length = 50
 
+    words = section_content.split()
+    if len(words) > 2:
+        seed = tuple(words[:order])
+    else:
+        return section_content
+
+    frequency_table = generate_frequency_table(section_content, order)
+    summary = generate_markov_text(frequency_table, seed, length)
+    return summary
+
+#Bullet point format
 def format_bullet_list(text):
     """With the help of chat gpt, I created this function
     to help me format the text into bullet points."""
@@ -75,13 +129,15 @@ def format_bullet_list(text):
     bullet_list = "\n\n".join(bullets)
     return bullet_list
 
-def sentiment_analysis(sentence):
-    """
-    this functions performs a sentiment analysis of a given sentence
-    """
-    from nltk.sentiment.vader import SentimentIntensityAnalyzer 
-    score = SentimentIntensityAnalyzer().polarity_scores(sentence)
-    print(score)
+#Didn't end up using the sentiment analysis function because my mac was giving me issues while downloading the software
+#I also realised sentiment analysis doesn't make too much sense in the case I have decided to develop
+# def sentiment_analysis(sentence):
+#     """
+#     this functions performs a sentiment analysis of a given sentence
+#     """
+#     from nltk.sentiment.vader import SentimentIntensityAnalyzer 
+#     score = SentimentIntensityAnalyzer().polarity_scores(sentence)
+#     print(score)
 
 def calling_section_analysis():
     """This function promts inputs from the user and calls the 
@@ -92,6 +148,7 @@ def calling_section_analysis():
         print(section_analysis(indexsec))
         answer = input('Do you want to analyse another section (yes/no):')
 
+#if main:
 def main():
     college_lookup()
     calling_section_analysis()
